f3293b62e1dc27756f73de81cecd033dca37a151
Casey Labs <sean@caseylabs.com>
2015-05-11 14:20:36 -0700

    Changing tabs to spaces (2 spaces)

--- check-snapshots.sh
+++ check-snapshots.sh
@@ -20,10 +20,10 @@ declare -r BINARIES=(logger echo date aws curl)
 declare PLACEMARK='lost'
 
 # Volumes must have a snapshot that is under $DAYS_MIN days old
-declare -ri DAYS_MIN=3	
+declare -ri DAYS_MIN=3  
 
 # $DAY_MIN converted into seconds
-declare -ri DAYS_MIN_SEC=$(date +%s --date "${DAYS_MIN} days ago")	
+declare -ri DAYS_MIN_SEC=$(date +%s --date "${DAYS_MIN} days ago")  
 
 # Get list of running instances
 declare INSTANCES=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters "Name=instance-state-name,Values=running" --output text)
@@ -35,7 +35,7 @@ declare REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/avail
 
 ## Function Declarations ##
 DoLog () {
-	local fail_flag=${2:-0}
+  local fail_flag=${2:-0}
         if (( ${LOG_LEVEL} > 0 ))
         then
                 #If debugging add some extra tags
@@ -57,38 +57,38 @@ DoLog () {
 
 # Confirm that the AWS CLI and related tools are installed.
 DepCheck() {
-	for prerequisite in ${BINARIES}; do
-		hash ${BINARIES} &> /dev/null
-		if [[ $? == 1 ]]; then
-			echo "In order to use this script, the executable \"$prerequisite\" must be installed." 1>&2; exit 70
-		fi
-	done
+  for prerequisite in ${BINARIES}; do
+    hash ${BINARIES} &> /dev/null
+    if [[ $? == 1 ]]; then
+      echo "In order to use this script, the executable \"$prerequisite\" must be installed." 1>&2; exit 70
+    fi
+  done
 }
 
 # Clean up temp files upon exiting the script.
 CleanUp() {
-	(( ${LOG_LEVEL} > 0 )) && echo -e "\nClean the things"
-	#Reset Bash variables
-	set +u +o pipefail
+  (( ${LOG_LEVEL} > 0 )) && echo -e "\nClean the things"
+  #Reset Bash variables
+  set +u +o pipefail
 }
 
 # Terminate the script.
 Terminate() {
-	MESSAGE=${1:-Terminating.}
-	RETURN=${2:-0}
-	if (( ${RETURN} > 0 ))
-	then
-		DoLog "${MESSAGE}" 1
-	else
-		DoLog "${MESSAGE}" 0
-	fi
-		
-	exit ${RETURN} 
+  MESSAGE=${1:-Terminating.}
+  RETURN=${2:-0}
+  if (( ${RETURN} > 0 ))
+  then
+    DoLog "${MESSAGE}" 1
+  else
+    DoLog "${MESSAGE}" 0
+  fi
+    
+  exit ${RETURN} 
 }
 
 # Send alert if snapshots are missing.
 DoAlert() {
-	DoLog "Alert! Instance: ${instance} (${description}) Volume: ${volume} has no recent snapshots." '1'
+  DoLog "Alert! Instance: ${instance} (${description}) Volume: ${volume} has no recent snapshots." '1'
 }
 
 # Trap function signal to trap (kill -l to list)
@@ -108,35 +108,35 @@ DoLog "There are ${INSTANCES_NUM} instances running."
 
 for instance in ${INSTANCES}
 do
-	description=$(aws ec2 describe-instances --region ${REGION} --instance-id ${instance} --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value[]')
-	
-	DoLog "Checking ${instance}: (${description})"
+  description=$(aws ec2 describe-instances --region ${REGION} --instance-id ${instance} --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value[]')
+  
+  DoLog "Checking ${instance}: (${description})"
 
     # Check launch date of instance
-	launch_time=$(aws ec2 describe-instances --region ${REGION} --instance-ids ${instance} --query 'Reservations[*].Instances[*].[LaunchTime]' --output text | sed 's/T.*$//')
-	launch_time_sec=$(date "--date=${launch_time}" +%s)
+  launch_time=$(aws ec2 describe-instances --region ${REGION} --instance-ids ${instance} --query 'Reservations[*].Instances[*].[LaunchTime]' --output text | sed 's/T.*$//')
+  launch_time_sec=$(date "--date=${launch_time}" +%s)
 
-	# Proceed if the instance launch date is greater than 3 days, otherwise exit. Why? Because we don't want alerts for recently launched instances.
-	[[ ${launch_time_sec} > ${DAYS_MIN_SEC} ]] && DoLog "${instance} (${description}) was launched less than $DAYS_MIN days ago, not alerting." && continue
-	
-	# Grab all volume IDs attached to this particular instance
-	volume_list=$(aws ec2 describe-volumes --region ${REGION} --filters Name=attachment.instance-id,Values=${instance} --query Volumes[].VolumeId --output text)
-			   
-	for volume in ${volume_list}; do
-		# Grab all snapshot associated with this particular volume, and find the most recent snapshot time
-		last_snap=$(aws ec2 describe-snapshots --region ${REGION} --output=text --filters "Name=volume-id,Values=${volume}" --query Snapshots[].[StartTime] | sed 's/T.*$//' | sort -u | tail -n1)
+  # Proceed if the instance launch date is greater than 3 days, otherwise exit. Why? Because we don't want alerts for recently launched instances.
+  [[ ${launch_time_sec} > ${DAYS_MIN_SEC} ]] && DoLog "${instance} (${description}) was launched less than $DAYS_MIN days ago, not alerting." && continue
+  
+  # Grab all volume IDs attached to this particular instance
+  volume_list=$(aws ec2 describe-volumes --region ${REGION} --filters Name=attachment.instance-id,Values=${instance} --query Volumes[].VolumeId --output text)
+         
+  for volume in ${volume_list}; do
+    # Grab all snapshot associated with this particular volume, and find the most recent snapshot time
+    last_snap=$(aws ec2 describe-snapshots --region ${REGION} --output=text --filters "Name=volume-id,Values=${volume}" --query Snapshots[].[StartTime] | sed 's/T.*$//' | sort -u | tail -n1)
 
-		if [[ -z ${last_snap} ]]; then
-			DoAlert
-		else
-			last_snap_sec=$(date "--date=${last_snap}" +%s)
-		
-			# If the latest snapshot is older than $DAYS_MIN, send an alert.
-			if [[ ${last_snap_sec} < ${DAYS_MIN_SEC} ]]; then 
-				DoAlert
-			fi
-		fi
-	done
+    if [[ -z ${last_snap} ]]; then
+      DoAlert
+    else
+      last_snap_sec=$(date "--date=${last_snap}" +%s)
+    
+      # If the latest snapshot is older than $DAYS_MIN, send an alert.
+      if [[ ${last_snap_sec} < ${DAYS_MIN_SEC} ]]; then 
+        DoAlert
+      fi
+    fi
+  done
 done
 
 
